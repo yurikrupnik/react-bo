733e36d1c155af0f4a835f2d9d3a925602bc6b59
yurikrupnik <krupnik.yuri@gmail.com>
2018-04-14 23:49:00 +0300

    test

---
+++ src/component/views/App/index.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Enxyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Component from './index';
+
+const { describe, it, expect } = global;
+
+Enxyme.configure({ adapter: new Adapter() });
+
+function setup() {
+    const props = { userAgent: 'dasd' };
+
+    const enzymeWrapper = mount(<Component {...props} />);
+
+    return {
+        props,
+        enzymeWrapper
+    };
+}
+
+
+describe('sda', () => {
+    it('should render', () => {
+        const { enzymeWrapper } = setup();
+        expect(enzymeWrapper.length).toBeTruthy();
+    });
+});
+
